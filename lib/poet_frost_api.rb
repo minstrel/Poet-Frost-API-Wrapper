5afcd659f8eaa6875f09bcb178062da14481254d
Donald Smith <donald@sinbissquare.com>
2018-02-12 15:37:48 -0500

    Fix RDoc comments

--- lib/poet_frost_api.rb
+++ lib/poet_frost_api.rb
@@ -2,27 +2,27 @@ require 'net/http'
 require 'json'
 require 'date'
 
+# To use any of the methods, register an API key at https://frost.po.et/
+# and save it as the environment variable FROST_TOKEN.
 module PoetFrostAPI
-  # To use any of the methods, register an API key at https://frost.po.et/
-  # and save it as the environment variable FROST_TOKEN.
 
   @@api_key = ENV['FROST_TOKEN']
   @@uri = URI('https://api.frost.po.et/works/')
   @@http = Net::HTTP.new(@@uri.host, @@uri.port)
   @@http.use_ssl = true
 
+  # Register a work on Po.et.
+  #
+  # Usage: PoetFrostAPI.create_work(name: 'Work Name',
+  #                                 datePublished: DateTime.now.iso8601,
+  #                                 dateCreated: DateTime.now.iso8601,
+  #                                 author: 'Author Name'
+  #                                 tags: 'Tag1, Tag2'
+  #                                 content: 'Content body'
+  #                                 )
+  #
+  # Returns a string with the workid that was registered.
   def PoetFrostAPI.create_work(args = {})
-    # Register a work on Po.et.
-    #
-    # Usage: PoetFrostAPI.create_work(name: 'Work Name',
-    #                                 datePublished: DateTime.now.iso8601,
-    #                                 dateCreated: DateTime.now.iso8601,
-    #                                 author: 'Author Name'
-    #                                 tags: 'Tag1, Tag2'
-    #                                 content: 'Content body'
-    #                                 )
-    #
-    # Returns a string with the workid that was registered.
 
     req = Net::HTTP::Post.new(@@uri.path)
     req.content_type = 'application/json'
@@ -40,14 +40,13 @@ module PoetFrostAPI
     puts "failed #{e}"
   end
 
+  # Retrieve a specific work from Po.et, using the workId returned from
+  # create_work.
+  #
+  # Usage: PoetFrostAPI.get_work(workId)
+  #
+  # Returns a hash with the created fields.
   def PoetFrostAPI.get_work(workId)
-    # Retrieve a specific work from Po.et, using the workId returned from
-    # create_work.
-    #
-    # Usage: PoetFrostAPI.get_work(workId)
-    #
-    # Returns a hash with the created fields.
-
     uri = @@uri + workId
     req = Net::HTTP::Get.new(uri.path)
     req.content_type = 'application/json'
@@ -58,10 +57,9 @@ module PoetFrostAPI
     puts "failed #{e}"
   end
 
+  # Retrieve all works submitted by your Frost API Token.
+  #
   def PoetFrostAPI.get_all_works
-    # Retrieve all works submitted by your Frost API Token.
-    #
-
     req = Net::HTTP::Get.new(@@uri.path)
     req.content_type = 'application/json'
     req['token'] = @@api_key
--- poet_frost_api.gemspec
+++ poet_frost_api.gemspec
@@ -1,5 +1,5 @@
 Gem::Specification.new do |s|
-  s.name        = 'Poet-Frost-API'
+  s.name        = 'poet_frost_API'
   s.version     = '0.0.1'
   s.date        = '2018-02-12'
   s.summary     = 'API Wrapper for Po.et Frost'
